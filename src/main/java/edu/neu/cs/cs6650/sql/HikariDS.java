b33d78498345fb015f05c33995b500f5bfffb335
QA Hoang <qahoang317@gmail.com>
2019-11-12 10:52:09 -0800

    Attempt to connect to Cloud SQL from GAE

--- src/main/java/edu/neu/cs/cs6650/sql/HikariDS.java
+++ src/main/java/edu/neu/cs/cs6650/sql/HikariDS.java
@@ -14,23 +14,29 @@ public class HikariDS {
 
   private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
   private static final String LOCALHOST_JDBC_URL = "jdbc:mysql://localhost:3306/skierapi?useSSL=false&serverTimezone=UTC";
-  private static final String SQL_CONN_USERNAME = IS_LOCAL ? "root" : System.getenv("RDS_USERNAME");
-  private static final String SQL_CONN_PW = IS_LOCAL ? "root" : System.getenv("RDS_PW");
+  private static final String JDBC_USERNAME = IS_LOCAL ? "root" : System.getenv("RDS_USERNAME");
+  private static final String JDBC_PW = IS_LOCAL ? "root" : System.getenv("RDS_PW");
 
   private static final String JDBC_URL =
       IS_LOCAL ? LOCALHOST_JDBC_URL
                : System.getenv("RDS_JDBC_URL");
+  private static final String GOOGLE_CLOUD_SQL_INSTANCE = System.getenv("CLOUD_SQL_INSTANCE");
 
   private static HikariDataSource dataSource = new HikariDataSource();
     // HikariCP uses milliseconds for all time values.
     static {
       logger.info("JDBC URL: " + JDBC_URL);
+      logger.info("JDBC_USERNAME: " + JDBC_USERNAME);
+      logger.info("JDBC_PW: " + JDBC_PW);
       dataSource.setJdbcUrl(JDBC_URL);
-      dataSource.setUsername(SQL_CONN_USERNAME);
-      dataSource.setPassword(SQL_CONN_PW);
+      dataSource.setUsername(JDBC_USERNAME);
+      dataSource.setPassword(JDBC_PW);
       dataSource.setMaximumPoolSize(20);
       dataSource.setDriverClassName(JDBC_DRIVER);
+      // controls the max time that a connection is allowed to sit idle in the pool
 //      dataSource.setIdleTimeout(28740000);
+      // controls the min number of idle connections HikariCP tries to maintain in the pool, including both idle and in-use connections
+//      dataSource.setMinimumIdle();
       dataSource.setMaxLifetime(120 * 1_000); // controls the maximum lifetime of a connection in the pool
 //      dataSource.setLeakDetectionThreshold(60000);
       dataSource.addDataSourceProperty("cachePrepStmts", "true");
@@ -43,6 +49,11 @@ public class HikariDS {
       dataSource.addDataSourceProperty("cacheServerConfiguration", "true");
       dataSource.addDataSourceProperty("elideSetAutoCommits", "true");
       dataSource.addDataSourceProperty("maintainTimeStats", "false");
+
+      // Google App Engine Settings
+      dataSource.addDataSourceProperty("socketFactory", "com.google.cloud.sql.mysql.SocketFactory");
+      dataSource.addDataSourceProperty("cloudSqlInstance", "cs6650-skierapi:us-west2:cs6650-skierapi");
+      dataSource.addDataSourceProperty("useSSL", "false");
     }
 
   public static Connection getConnection() throws SQLException {
